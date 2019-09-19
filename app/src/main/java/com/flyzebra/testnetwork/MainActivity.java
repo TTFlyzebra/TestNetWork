5bb18897b033e6f3ed61b88fc5f95609a3520082
flyzebra <flycnzebra@gmail.com>
2019-09-19 21:33:12 +0800

    first commit

--- app/src/main/java/com/flyzebra/testnetwork/MainActivity.java
+++ app/src/main/java/com/flyzebra/testnetwork/MainActivity.java
@@ -2,38 +2,48 @@ package com.flyzebra.testnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private ConnectivityManager cm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        cm = (ConnectivityManager) MainActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("TTTT", "onCreate");
+                Network[] networks = cm.getAllNetworks();
+                Log.d("TTTT", "networks="+networks+",size="+networks.length);
+                boolean result = false;
+                for (int i = 0; i < networks.length; i++) {
+                    NetworkInfo netInfo = cm.getNetworkInfo(networks[i]);
+                    Log.d("TTTT", "type="+netInfo.getType());
+                    if (netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (Build.VERSION.SDK_INT >= 23) {
+                            result =cm.bindProcessToNetwork(networks[i]);
+                        } else {
+                            // 23后这个方法舍弃了
+                            result =ConnectivityManager.setProcessDefaultNetwork(networks[i]);
+                        }
+                        Log.d("TTTT", "Result:" + result);
+                    }
+                    if (result) {
+                        Log.d("TTTT", "Success!Restrictedto:" + netInfo.toString());
+                        break;
+                    }
+                }
+            }
+        }).start();
 
-        Log.d("TTTT", "onCreate");
-        Network[] networks = cm.getAllNetworks();
-        Log.d("TTTT", "networks="+networks+",size="+networks.length);
-        boolean result = false;
-        for (int i = 0; i < networks.length; i++) {
-            NetworkInfo netInfo = cm.getNetworkInfo(networks[i]);
-            Log.d("TTTT", "type="+netInfo.getType());
-            if (netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                Log.d("TTTT", "Foundpotentialnetwork:settingdefault...");
-                result = ConnectivityManager.setProcessDefaultNetwork(networks[i]);
-                Log.d("TTTT", "Result:" + result);
-            }
-            if (result) {
-                Log.d("TTTT", "Success!Restrictedto:" + netInfo.toString());
-                break;
-            }
-        }
     }
 }
